dfedd1bce8ebdc6dd548d4698ce71d3a31722449
mmcdon15 <mmcdon15@gmail.com>
2025-10-26 04:26:29 -0400

    B: circles!

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt =0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -13,5 +15,6 @@ def main():
                 return
         pygame.Surface.fill(screen,(0,0,0))
         pygame.display.flip()
+        dt=clock.tick(60)/1000
 if __name__ == "__main__":
     main()
