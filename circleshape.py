0b39ebc39f76f880106cecb89eb4e3dab1ab922d
mmcdon15 <mmcdon15@gmail.com>
2025-10-27 03:55:41 -0400

    D: collisions!

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,7 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def collision_check(self, player):
+        return self.position.distance_to(player.position)<=(player.radius)
+  
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
+import sys
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     print("Starting Asteroids!")
@@ -10,11 +13,14 @@ def main():
     clock = pygame.time.Clock()
     dt =0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    asteroids = pygame.sprite.Group()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     player1=Player((SCREEN_WIDTH/2),(SCREEN_HEIGHT/2))
+    asteroid_field = AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -23,6 +29,10 @@ def main():
         for spr in drawable:
             spr.draw(screen)
         updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collision_check(player1):
+                print("Game Over!")
+                sys.exit()
         #screen.fill((0, 0, 0))
         pygame.display.flip()
         dt=clock.tick(60)/1000
